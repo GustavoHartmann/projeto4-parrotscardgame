c1320b137f38f52991304038a51e0f97593cefac
GustavoHartmann <gustavolerner.hartmann@gmail.com>
2022-06-20 18:28:26 -0300

    Versão Final

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let qtd = prompt("Com quantas cartas você deseja jogar?")
+let contador = 0;
+let name1;
+let name2;
+let qtd = prompt("Com quantas cartas você deseja jogar?");
 
 while(qtd % 2 !== 0 || qtd > 14 || qtd < 4) {
-    qtd = prompt("Com quantas cartas você deseja jogar?")
+    qtd = prompt("Com quantas cartas você deseja jogar?");
 }
 
-let gifs = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit", "triplets", "triplets", "unicorn", "unicorn"]
+let gifs = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit", "triplets", "triplets", "unicorn", "unicorn"];
 
 let randomGifs = [];
 
@@ -17,11 +20,47 @@ randomGifs.sort(comparador);
 for(let i = 0; i < qtd; i++) {
     document.querySelector(".cards").innerHTML += `
             <div class="card" onclick="virarCarta(this)">
-                <img src="./IMG/${randomGifs[i]}parrot.gif" class="frente">
+                <img src="./IMG/${randomGifs[i]}parrot.gif" class="hide frente">
+                <img src="./IMG/front.png" class="verso">
             </div>
     `
 }
 
 function comparador() { 
 	return Math.random() - 0.5;
+}
+
+function virarCarta(element) {
+    element.classList.add("turn")
+    const verso = element.querySelector(".verso");
+    const frente = element.querySelector(".frente");
+    if(frente.classList.contains("hide")){
+        verso.classList.add("hide");
+        frente.classList.remove("hide");
+    }
+     
+    contadorCarta(element);
+    setTimeout(desvirarCarta(element), 1000)
+}
+
+function contadorCarta(elemento) {
+    contador++;
+    if(contador === 1) {
+        name1 = elemento.querySelector("img").src
+    } else if(contador === 2) {
+        name2 = elemento.querySelector("img").src
+    } else {
+        contador = 0;
+    }
+    
+}
+
+function desvirarCarta(elemento) {
+    const verso = elemento.querySelector(".verso")
+    const frente = elemento.querySelector(".frente")
+    if(name1 !== name2) {
+        verso.classList.add("hide")
+        frente.classList.remove("hide")
+    }
+    
 }
